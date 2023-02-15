eb4373682c7c3eb55f3965ffa38f5c8f896edce0
sunyy01 <syy200271@gmail.com>
2023-02-15 15:05:36 -0800

    add queries and edit add method and crash

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -11,6 +11,7 @@ import {Section} from "./CourseHelper";
 import {Dataset} from "./DatasetHelper";
 import * as fs from "fs-extra";
 import {CheckQuery} from "./CheckQuery";
+import {SearchQuery} from "./SearchQuery";
 
 
 /**
@@ -83,7 +84,7 @@ export default class InsightFacade implements IInsightFacade {
 
 	public removeDataset(id: string): Promise<string> {
 		return new Promise((fulfill, reject) => {
-			if (id === "" || id === " " || id.includes("_")) {
+			if (id === "" || (!id.trim()) || id.includes("_")) {
 				return reject(new InsightError("Invalid Dataset ID!"));
 			}
 			if (!InsightFacade.IDs.includes(id)) {
@@ -103,7 +104,99 @@ export default class InsightFacade implements IInsightFacade {
 	}
 
 	public performQuery(que: unknown): Promise<InsightResult[]> {
-		return Promise.reject("true");
+		return new Promise((resolve, reject) => {
+			let query: CheckQuery;
+			query = new CheckQuery(InsightFacade.IDs);
+			let quer: any = que;
+			let search: SearchQuery;
+			query.checkQuery(que).then(() => {
+				search = new SearchQuery(quer.WHERE, InsightFacade.datasets.get(query.getDataset()));
+				return search.searchQuery();
+			}).then((sec) => {
+				// console.log(sec);
+				return resolve(this.displayQuery(sec, quer, query.getDataset()));
+			}).catch((err) => {
+				return reject(err);
+			});
+		});
+	}
+
+	private displayQuery(secs: Section[], query: any, id: string): InsightResult[] {
+		let arr: any[] = [];
+		let result = this.displaySections(secs, query, id, arr);
+		// console.log("here");
+		// console.log(query);
+		// console.log(query.OPTIONS);
+		return this.displayOrder(result, query.OPTIONS);
+	}
+	private displaySections(secs: Section[], query: any, id: string, arr: any[]): InsightResult[] {
+		let keys = query.OPTIONS.COLUMNS;
+		for (const sec of secs) {
+			let obj = this.displaySection(sec, keys, id);
+			// console.log(obj);
+			arr.push(obj);
+		}
+		return arr;
+	}
+	private displaySection(sec: Section, keys: string[], id: string): InsightResult {
+		let obj = Object.create(null);
+		for(const key of keys) {
+			if (key === id.concat("_uuid")) {
+				obj[key] = sec.uuid;
+			} else if (key === id.concat("_year")) {
+				obj[key] = sec.year;
+			} else if (key === id.concat("_dept")) {
+				obj[key] = sec.dept;
+			} else if (key === id.concat("_id")) {
+				obj[key] = sec.id;
+			} else if (key === id.concat("_title")) {
+				obj[key] = sec.title;
+			} else if (key === id.concat("_instructor")) {
+				obj[key] = sec.instructor;
+			} else if (key === id.concat("_avg")) {
+				obj[id.concat("_avg")] = sec.avg;
+			} else if (key === id.concat("_pass")) {
+				obj[key] = sec.pass;
+			} else if (key === id.concat("_fail")) {
+				obj[key] = sec.fail;
+			} else {
+				obj[key] = sec.audit;
+			}
+		}
+		return obj;
+	}
+
+	private displayOrder(secs: any[], obj: any): InsightResult[] {
+		// console.log("order");
+		// console.log(obj);
+		if (obj["ORDER"] === null) {
+			return secs;
+		} else {
+			for (let i = 0; i < secs.length; i++) {
+				let min = this.findMin(secs, i, obj.ORDER);
+				let s = secs[i];
+				secs[i] = secs[min];
+				secs[min] = s;
+			}
+			return secs;
+		}
+	}
+	private findMin(secs: any[], i: number, str: string): number {
+		let j = i;
+		let min: number = i;
+		while (j !== secs.length) {
+			if (typeof secs[j][str] === "string") {
+				if (secs[j][str].localeCompare(secs[min][str]) <= 0) {
+					min = j;
+				}
+			} else {
+				if (secs[j][str] <= secs[min][str]) {
+					min = j;
+				}
+			}
+			j++;
+		}
+		return min;
 	}
 
 	public listDatasets(): Promise<InsightDataset[]> {
@@ -161,10 +254,14 @@ export default class InsightFacade implements IInsightFacade {
 			}
 		}
 	}
+
 	private static checkValidID(ID: string, kind: InsightDatasetKind): boolean {
 		return (
+			// empty string case
+			ID === null ||
+			ID === undefined ||
+			(!ID.trim()) ||
 			ID === "" ||
-			ID === " " ||
 			ID.includes("_") ||
 			InsightFacade.IDs.includes(ID) ||
 			fs.existsSync("./data/" + ID + ".json") ||
@@ -183,8 +280,17 @@ export default class InsightFacade implements IInsightFacade {
 				}
 				if (!datasets.has(id)) {
 					const obj = fs.readJsonSync("./data/" + id + ".json");
-					const dataset = new Dataset(id, obj.sections);
-					datasets.set(id, dataset);
+					// const dataset = new Dataset(id, obj.sections);
+					// datasets.set(id, dataset);
+					const sections: Section[] = [];
+					for (const sec of obj.sections){
+						console.log(sec);
+						const section = new Section(sec.uuid, sec.id, sec.title, sec.instructor,
+							sec.dept, sec.year, sec.avg,sec.pass, sec.fail,sec.audit);
+						sections.push(section);
+					}
+					const dataset = new Dataset(id,sections);
+					datasets.set(id,dataset);
 				}
 			});
 		}
