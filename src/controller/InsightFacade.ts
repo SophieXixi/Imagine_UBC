e7930fc6ff4a239d90ffd2ac1250c3680cef5617
sophie <yren06@student.ubc.ca>
2023-03-16 23:42:17 -0700

    #10 identify id

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -17,7 +17,6 @@ import {DisplayRoom} from "./DisplayRoom";
 import {CheckSection} from "./CheckSection";
 import {SearchSection} from "./SearchSection";
 import {Room} from "./RoomHelper";
-import {time} from "console";
 
 /**
  * This is the main programmatic entry point for the project.
@@ -80,15 +79,8 @@ export default class InsightFacade implements IInsightFacade {
 		return new Promise((resolve, reject) => {
 			let quer: any = que;
 			let id, search, display;
-			if (quer.OPTIONS && quer.OPTIONS.COLUMNS && Array.isArray(quer.OPTIONS.COLUMNS) &&
-				quer.OPTIONS.COLUMNS.length !== 0) {
-				let div = quer.OPTIONS.COLUMNS[0].search("_");
-				if (div > 0) {
-					id = quer.OPTIONS.COLUMNS[0].substring(0, div);
-				} else {
-					return reject(new InsightError("invalid"));
-				}
-			} else {
+			id = this.searchID(quer);
+			if (id === "0") {
 				return reject(new InsightError("invalid"));
 			}
 			let ds = InsightFacade.datasets.get(id);
@@ -121,6 +113,28 @@ export default class InsightFacade implements IInsightFacade {
 					});
 			}
 		});
+	}
+
+	private searchID(quer: any): string {
+		if (quer.OPTIONS && quer.OPTIONS.COLUMNS && Array.isArray(quer.OPTIONS.COLUMNS) &&
+			quer.OPTIONS.COLUMNS.length !== 0) {
+			let div = quer.OPTIONS.COLUMNS[0].search("_");
+			if (div > 0) {
+				return quer.OPTIONS.COLUMNS[0].substring(0, div);
+			} else if (quer.TRANSFORMATIONS && quer.TRANSFORMATIONS.GROUP &&
+				Array.isArray(quer.TRANSFORMATIONS.GROUP) && quer.TRANSFORMATIONS.GROUP.length !== 0) {
+				let i = quer.TRANSFORMATIONS.GROUP[0].search("_");
+				if (i > 0) {
+					return quer.TRANSFORMATIONS.GROUP[0].substring(0, i);
+				} else {
+					return "0";
+				}
+			} else {
+				return "0";
+			}
+		} else {
+			return "0";
+		}
 	}
 
 	public listDatasets(): Promise<InsightDataset[]> {
